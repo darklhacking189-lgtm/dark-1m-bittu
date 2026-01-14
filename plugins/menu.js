5f18c015deb70d5e8e22c535c75a8bc375a9738d
Mr-rabbit-xd <bihh5539@gmail.com>
2026-01-14 12:45:09 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -82,7 +82,7 @@ ${readMore}
     }
 
     const opts = {
-      image: { url: "https://www.rabbit.zone.id/pzf1km.jpg" },
+      image: { url: "https://ibb.co/TMT4SmJ9" },
       caption: _cmd_st,
       mimetype: "image/jpeg",
       contextInfo: {
